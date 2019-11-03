4b4d2a5cd90bdc42559f5fd6244634b12c840c09
Andy Bettisworth <andy@accreu.com>
2019-11-03 17:56:10 -0600

    Add filtering to links

--- python/hackernews/links/schema.py
+++ python/hackernews/links/schema.py
@@ -1,6 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
 from graphql import GraphQLError
+from django.db.models import Q
 
 from users.schema import UserType
 
@@ -21,10 +22,17 @@ class VoteType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    links = graphene.List(LinkType)
+    links = graphene.List(LinkType, search=graphene.String())
     votes = graphene.List(VoteType)
 
-    def resolve_links(self, info, **kwargs):
+    def resolve_links(self, info, search=None, **kwargs):
+        if search:
+            filter = (
+                Q(url__icontains=search) |
+                Q(description__icontains=search)
+            )
+            return Link.objects.filter(filter)
+
         return Link.objects.all()
 
     def resolve_votes(self, info, **kwargs):
