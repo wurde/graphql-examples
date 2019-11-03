8bf33605df72b02c6d7e27b2dc54ffd6353b7778
Andy Bettisworth <andy@accreu.com>
2019-11-03 00:41:56 -0500

    Add Votes

--- ruby/app/graphql/types/mutation_type.rb
+++ ruby/app/graphql/types/mutation_type.rb
@@ -2,6 +2,7 @@ module Types
   class MutationType < Types::BaseObject
     field :create_link, mutation: Mutations::CreateLink
     field :create_user, mutation: Mutations::CreateUser
+    field :create_vote, mutation: Mutations::CreateVote
     field :sign_in_user, mutation: Mutations::SignInUser
   end
 end
