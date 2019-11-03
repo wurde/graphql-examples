273cca4b757a70d921c34abb44019fda9219804f
Andy Bettisworth <andy@accreu.com>
2019-11-02 20:56:43 -0500

    Test usage of CreateLink

--- ruby/app/graphql/types/mutation_type.rb
+++ ruby/app/graphql/types/mutation_type.rb
@@ -1,11 +1,5 @@
 module Types
   class MutationType < Types::BaseObject
-    field :create_link, LinkType, null: false,
-      description: "Create a LinkType object."
-
-      def create_link
-        Link.create(url: 'http://example.com', description: '')
-      end
-    end
+    field :create_link, mutation: Mutations::CreateLink
   end
 end
