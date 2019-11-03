1c3232c91b3c113acf2e93af4d6685bdf1ea6dd3
Andy Bettisworth <andy@accreu.com>
2019-11-02 20:50:34 -0500

    Add CreateLink mutation

--- ruby/app/graphql/types/mutation_type.rb
+++ ruby/app/graphql/types/mutation_type.rb
@@ -1,10 +1,11 @@
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
+    field :create_link, LinkType, null: false,
+      description: "Create a LinkType object."
+
+      def create_link
+        Link.create(url: 'http://example.com', description: '')
+      end
     end
   end
 end
