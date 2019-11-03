2743cfbe073ffb20cedcd8cbb885bc026e6d4039
Andy Bettisworth <andy@accreu.com>
2019-11-03 14:43:06 -0600

    Add advanced filtering to allLinks field

--- ruby/app/graphql/types/query_type.rb
+++ ruby/app/graphql/types/query_type.rb
@@ -3,13 +3,8 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
     # `all_links` is automatically camelcased to `allLinks`.
-    field :all_links, [LinkType], null: false
+    field :all_links, function: Resolvers::LinksSearch
     field :all_users, [UserType], null: false
-
-    # this method is invoked, when `all_link` fields is being resolved
-    def all_links
-      Link.all
-    end
 
     def all_users
       User.all
