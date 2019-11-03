f9a42acd716ddef3c41c5c469601c362e4a3db91
Andy Bettisworth <andy@accreu.com>
2019-11-03 17:39:53 -0600

    Associate Links with Users

--- python/hackernews/links/schema.py
+++ python/hackernews/links/schema.py
@@ -1,6 +1,8 @@
 import graphene
 from graphene_django import DjangoObjectType
 
+from users.schema import UserType
+
 from .models import Link
 
 # 
@@ -26,19 +28,27 @@ class CreateLink(graphene.Mutation):
     id = graphene.Int()
     url = graphene.String()
     description = graphene.String()
+    posted_by = graphene.Field(UserType)
 
     class Arguments:
         url = graphene.String()
         description = graphene.String()
 
     def mutate(self, info, url, description):
-        link = Link(url=url, description=description)
+        user = info.context.user or None
+
+        link = Link(
+            url=url,
+            description=description,
+            posted_by=user
+        )
         link.save()
 
         return CreateLink(
             id=link.id,
             url=link.url,
             description=link.description,
+            posted_by=link.posted_by
         )
 
 class Mutation(graphene.ObjectType):
