234e7df961271ede9de519973773ef3866ea6a6e
Andy Bettisworth <andy@accreu.com>
2019-11-02 23:04:18 -0500

    Add SignInUser mutation

--- ruby/app/graphql/types/mutation_type.rb
+++ ruby/app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_link, mutation: Mutations::CreateLink
     field :create_user, mutation: Mutations::CreateUser
+    field :sign_in_user, mutation: Mutations::SignInUser
   end
 end
