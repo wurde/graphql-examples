1d75473df3000b7a7c3da4dd101d4dca0d2a2640
Andy Bettisworth <andy@accreu.com>
2019-11-04 17:50:27 -0600

    Add allUsers Query field

--- node/src/resolvers/Query.js
+++ node/src/resolvers/Query.js
@@ -10,6 +10,10 @@ exports.allLinks = (parent, args, context) => {
   return context.prisma.links()
 }
 
+exports.allUsers = (parent, args, context) => {
+  return context.prisma.users()
+}
+
 exports.findLink = (parent, args, context) => {
   // return links.filter(link => link.id === parseInt(args.id))[0]
   return 'TODO findLink'
