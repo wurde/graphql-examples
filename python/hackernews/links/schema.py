5c53ae752df2fbc07957b9dd63188750466ff54e
Andy Bettisworth <andy@accreu.com>
2019-11-03 18:00:12 -0600

    Allow paginating results

--- python/hackernews/links/schema.py
+++ python/hackernews/links/schema.py
@@ -22,27 +22,40 @@ class VoteType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    links = graphene.List(LinkType, search=graphene.String())
+    links = graphene.List(
+        LinkType,
+        search=graphene.String(),
+        first=graphene.Int(),
+        skip=graphene.Int(),
+    )
     votes = graphene.List(VoteType)
 
-    def resolve_links(self, info, search=None, **kwargs):
+    def resolve_links(self, info, search=None, first=None, skip=None, **kwargs):
+        qs = Link.objects.all()
+
         if search:
             filter = (
                 Q(url__icontains=search) |
                 Q(description__icontains=search)
             )
-            return Link.objects.filter(filter)
+            qs = qs.filter(filter)
 
-        return Link.objects.all()
+        if skip:
+            qs = qs[skip:]
+
+        if first:
+            qs = qs[:first]
+
+        return qs
 
     def resolve_votes(self, info, **kwargs):
         return Vote.objects.all()
 
 #
 # Define mutation classes.
-#
 
 class CreateLink(graphene.Mutation):
+#
     id = graphene.Int()
     url = graphene.String()
     description = graphene.String()
