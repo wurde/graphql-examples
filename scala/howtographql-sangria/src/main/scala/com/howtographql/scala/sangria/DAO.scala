b7ce159bc587dfd3dab6706013fcfc2e9215416c
Andy Bettisworth <andy@accreu.com>
2019-11-03 20:37:17 -0600

    Add link and links fields to QueryType

--- scala/howtographql-sangria/src/main/scala/com/howtographql/scala/sangria/DAO.scala
+++ scala/howtographql-sangria/src/main/scala/com/howtographql/scala/sangria/DAO.scala
@@ -1,8 +1,19 @@
 package com.howtographql.scala.sangria
 
-import DBSchema._
 import slick.jdbc.H2Profile.api._
+import scala.concurrent.Future 
+
+import com.howtographql.scala.sangria.models.Link
+import DBSchema._
 
 class DAO(db: Database) {
   def allLinks = db.run(Links.result)
+
+  def getLink(id: Int): Future[Option[Link]] = db.run(
+    Links.filter(_.id === id).result.headOption
+  )
+
+  def getLinks(ids: Seq[Int]) = db.run(
+    Links.filter(_.id inSet ids).result
+  )
 }
--- scala/howtographql-sangria/src/main/scala/com/howtographql/scala/sangria/GraphQLSchema.scala
+++ scala/howtographql-sangria/src/main/scala/com/howtographql/scala/sangria/GraphQLSchema.scala
@@ -7,18 +7,31 @@ import sangria.macros.derive._
 
 object GraphQLSchema {
   val LinkType = ObjectType[Unit, Link](
-      "Link",
-      fields[Unit, Link](
-        Field("id", IntType, resolve = _.value.id),
-        Field("url", StringType, resolve = _.value.url),
-        Field("description", StringType, resolve = _.value.description)
-      )
+    "Link",
+    fields[Unit, Link](
+      Field("id", IntType, resolve = _.value.id),
+      Field("url", StringType, resolve = _.value.url),
+      Field("description", StringType, resolve = _.value.description)
     )
+  )
+
+  val Id = Argument("id", IntType)
+  val Ids = Argument("ids", ListInputType(IntType))
 
   val QueryType = ObjectType(
     "Query",
     fields[MyContext, Unit](
-      Field("allLinks", ListType(LinkType), resolve = c => c.ctx.dao.allLinks)
+      Field("allLinks", ListType(LinkType), resolve = c => c.ctx.dao.allLinks),
+      Field("link",
+        OptionType(LinkType),
+        arguments = Id :: Nil,
+        resolve = c => c.ctx.dao.getLink(c.arg(Id))
+      ),
+      Field("links",
+        ListType(LinkType),
+        arguments = Ids :: Nil,
+        resolve = c => c.ctx.dao.getLinks(c.arg(Ids))
+      )
     )
   )
 
