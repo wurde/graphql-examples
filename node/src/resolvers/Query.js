61d4a64f7065ade6e5d0df2056cb8d7be75d782a
Andy Bettisworth <andy@accreu.com>
2019-11-04 18:48:50 -0600

    Add Limit-Offset pagination

--- node/src/resolvers/Query.js
+++ node/src/resolvers/Query.js
@@ -22,3 +22,19 @@ exports.findLink = (parent, args, context) => {
   // return links.filter(link => link.id === parseInt(args.id))[0]
   return 'TODO findLink'
 }
+
+exports.feed = async (parent, args, context, info) => {
+  const where = args.filter ? {
+    OR: [
+      { description_contains: args.filter },
+      { url_contains: args.filter },
+    ],
+  } : {}
+
+  const links = await context.prisma.links({
+    where,
+    skip: args.skip,
+    first: args.first
+  })
+  return links
+}
