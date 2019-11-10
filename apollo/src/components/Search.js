59a5dd318f0bceeb4432fbbb457645cd4aea7a68
Andy Bettisworth <andy@accreu.com>
2019-11-09 23:01:59 -0600

    Install apollo-link-ws subscriptions-transport-ws

--- apollo/src/components/Search.js
+++ apollo/src/components/Search.js
@@ -8,6 +8,33 @@ import gql from 'graphql-tag';
 import Link from './Link';
 
 /**
+ * Define queries
+ */
+
+const FEED_SEARCH_QUERY = gql`
+  query FeedSearchQuery($filter: String!) {
+    feed(filter: $filter) {
+      links {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`;
+
+/**
  * Define component
  */
 
@@ -18,7 +45,13 @@ class Search extends Component {
   };
 
   executeSearch = async () => {
-    // ... you'll implement this 🔜
+    const { filter } = this.state;
+    const result = await this.props.client.query({
+      query: FEED_SEARCH_QUERY,
+      variables: { filter }
+    });
+    const links = result.data.feed.links;
+    this.setState({ links });
   };
 
   render() {
