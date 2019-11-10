ea98f0df075c26467ef23cffab48a3e4f8e04ba4
Andy Bettisworth <andy@accreu.com>
2019-11-09 19:15:26 -0600

    Configure the Apollo client

--- apollo/src/App.js
+++ apollo/src/App.js
@@ -1,11 +1,42 @@
+/**
+ * Dependencies
+ */
+
 import React from 'react';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+/**
+ * Define helpers
+ */
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
+/**
+ * Define component
+ */
 
 function App() {
   return (
-    <div>
-      Apollo
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        Apollo
+      </div>
+    </ApolloProvider>
   );
 }
+
+/**
+ * Export component
+ */
 
 export default App;
