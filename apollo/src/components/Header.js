c7c29447e9360f155423da67a6a2b2bc7c5f2d3d
Andy Bettisworth <andy@accreu.com>
2019-11-09 20:25:30 -0600

    Define Header component

---
+++ apollo/src/components/Header.js
@@ -0,0 +1,36 @@
+/**
+ * Dependencies
+ */
+
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router';
+
+/**
+ * Define component
+ */
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="flex pa1 justify-between nowrap orange">
+        <div className="flex flex-fixed black">
+          <div className="fw7 mr1">Hacker News</div>
+          <Link to="/" className="ml1 no-underline black">
+            new
+          </Link>
+          <div className="ml1">|</div>
+          <Link to="/create" className="ml1 no-underline black">
+            submit
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+/**
+ * Define component
+ */
+
+export default withRouter(Header);
