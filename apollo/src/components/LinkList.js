99751a43e496956b21dcbb7784ca83dd3fdf2c2a
Andy Bettisworth <andy@accreu.com>
2019-11-09 22:03:51 -0600

    Update cache on vote

--- apollo/src/components/LinkList.js
+++ apollo/src/components/LinkList.js
@@ -39,19 +39,33 @@ const FEED_QUERY = gql`
  */
 
 class LinkList extends Component {
+  updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY });
+
+    const votedLink = data.feed.links.find(link => link.id === linkId);
+    votedLink.votes = createVote.link.votes;
+
+    store.writeQuery({ query: FEED_QUERY, data });
+  };
+
   render() {
     return (
       <Query query={FEED_QUERY}>
         {({ loading, data, error }) => {
-          if (loading) return <div>Fetching</div>
-          if (error) return <div>Error</div>
+          if (loading) return <div>Fetching</div>;
+          if (error) return <div>Error</div>;
 
-          const linksToRender = data.feed.links
+          const linksToRender = data.feed.links;
 
           return (
             <div>
               {linksToRender.map((link, index) => (
-                <Link key={link.id} link={link} index={index} />
+                <Link
+                  key={link.id}
+                  link={link}
+                  index={index}
+                  updateCacheAfterVote={this.updateCacheAfterVote}
+                />
               ))}
             </div>
           );
